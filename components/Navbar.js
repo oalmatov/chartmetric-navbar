940aa4950648f1ae26a5a635b64d12c7d1d0a39c
Omar Almatov <omar.almatov@berkeley.edu>
2022-02-06 16:33:02 -0800

    added SearchBar

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,6 +4,7 @@ import NavLink from './NavLink';
 
 import { FaBullhorn, FaRegBell, FaBars, FaSearch } from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib';
+import SearchBar from './SearchBar';
 
 export default function Navbar() {
     return (
@@ -11,20 +12,10 @@ export default function Navbar() {
             <nav className="p-2 mx-auto">
                 <div className="flex justify-between">
                     <div>
-                        <Link href="/">
-                            <a>
-                                <Logo width="30px" height="30px" />
-                            </a>
-                        </Link>
+                        <Logo width="30px" height="30px" />
                     </div>
                     <div className="flex items-center space-x-5">
-                        <div className="flex items-center bg-white rounded-sm px-3">
-                            <FaSearch size="15px" className="fill-gray-500" />
-                            <input
-                                placeholder='Search ("/" for hotkey)'
-                                className="focus:outline-0 py-1.5 px-3 rounded-sm items-center text-gray-500 text-sm"
-                            />
-                        </div>
+                        <SearchBar />
                         <NavLink icon={<FaBullhorn />} />
                         <NavLink icon={<FaRegBell />} />
                         <NavLink icon={<FaBars />} />
